81d8109636d3def57dd8934c79c4a0ae7f2baed3
juanconizza <juanconizza@gmail.com>
2024-04-10 16:00:38 -0300

    sprint 4 terminado y testeado

--- src/middlewares/uploader.js
+++ src/middlewares/uploader.js
@@ -4,14 +4,16 @@ import __dirname from "../../utils.js";
 
 // Configuración de Multer
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, path.join(__dirname, "public/img/products"));
-    },
-    filename: (req, file, cb) => {
-      const filename = req.body.title.replace(/\s/g, "_") + path.extname(file.originalname);
-      cb(null, filename);
-    }
-  });
-  
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "public/img/products"));
+  },
+  filename: (req, file, cb) => {
+    // Obtener la extensión del archivo
+    const extname = path.extname(file.originalname);
+    // Generar un nombre único usando la fecha actual y la extensión del archivo
+    const filename = `${req.body.title.replace(/\s/g, "-")}_${Date.now()}${extname}`;
+    cb(null, filename);
+  }
+});
 
-  export const upload = multer({ storage: storage });
+export const upload = multer({ storage: storage });
