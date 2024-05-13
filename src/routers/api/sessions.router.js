25b2aab03d7ed8cc462b41a41f74a84323be652d
juanconizza <juanconizza@gmail.com>
2024-05-13 18:49:47 -0300

    Login terminado y funcionando, falta condicionar la barra

--- src/routers/api/sessions.router.js
+++ src/routers/api/sessions.router.js
@@ -2,6 +2,10 @@ import { Router } from "express";
 import userManager from "../../data/mongo/managers/UsersManager.mongo.js";
 import validateUsersProps from "../../middlewares/validateUsersProps.js";
 import isValidEmail from "../../middlewares/isValidEmail.js";
+import isValidUser from "../../middlewares/isValidUser.js";
+import isValidPass from "../../middlewares/isValidPass.js";
+import session from "express-session";
+ 
 
 const sessionRouter = new Router();
 
@@ -23,6 +27,33 @@ sessionRouter.post(
           message: "User created successfully!",
         });
         
+      } catch (error) {
+        return next(error);
+      }
+    }
+  );
+  
+  sessionRouter.post(
+    "/login",
+    isValidUser,
+    isValidPass,
+    async (req, res, next) => { 
+      try {
+        const { email } = req.body;
+        const user = await userManager.readByEmail(email);
+        
+        // Establecer variables de sesión
+        req.session.email = email;
+        req.session.online = true;
+        req.session.role = user.role;
+        req.session.user_id = user._id;
+        
+        // Enviar respuesta con el ID de usuario
+        return res.json({ 
+          statusCode: 200, 
+          message: "Logged in!",
+          user_id: user._id // Aquí se incluye el ID de usuario en la respuesta
+        });
       } catch (error) {
         return next(error);
       }
