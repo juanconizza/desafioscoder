d41d1aaeb1d23690e946c0def63b6716dcb32d46
juanconizza <juanconizza@gmail.com>
2024-08-30 17:00:37 -0300

    Fixes finales

--- test/supertest/app.test.js
+++ test/supertest/app.test.js
@@ -4,13 +4,12 @@ import environment from "../../src/utils/env.utils.js";
 import usersRepository from "../../src/repositories/users.rep.js";
 import productsRepository from "../../src/repositories/products.rep.js";
 
-// PARA PODER USAR EL SUPERTEST Debe modificar en el dto en la propiedad verify agregar "data.verify || false".
-
 const requester = supertest(`http://localhost:${environment.PORT}/api`);
 
 describe("Testing de MANANTIALES MARKET API", function () {
   this.timeout(20000);
-  const user = {
+
+  const userAdmin = {
     name: "Ana Esther",
     lastName: "Schreiner",
     dni: 327220002,
@@ -18,28 +17,46 @@ describe("Testing de MANANTIALES MARKET API", function () {
     phone: 3516829095,
     email: "anatesting@gmail.com",
     password: "Audio123",
+    role: 1,
     verify: true,
   };
+
+  const user = {
+    name: "Roberto",
+    lastName: "Sinderecho",
+    dni: 25451223,
+    blockAndLot: "4401",
+    phone: 3516957845,
+    email: "roberto-sinderecho@gmail.com",
+    password: "Audio123",
+    verify: true,
+  };
+
   const product = {
     title: "Producto de Prueba Testing",
     photo: "default_picture.png",
     category: "Electrónica",
-    description: "",
+    description: "producto de prueba en testing",
     price: 5000,
     stock: 3,
   };
+
   let token = "";
+  let adminToken = "";
+
   it("Registro de un usuario", async () => {
     const response = await requester.post("/sessions/register").send(user);
     const { _body } = response;
     expect(_body.statusCode).to.be.equals(201);
   });
+
   it("Inicio de sesión de un usuario", async () => {
     const response = await requester.post("/sessions/login").send(user);
     const { _body, headers } = response;
     token = headers["set-cookie"][0].split(";")[0];
     expect(_body.statusCode).to.be.equals(200);
   });
+
   it("Creación de un producto por parte del usuario", async () => {
     const response = await requester
       .post("/products")
@@ -48,6 +65,7 @@ describe("Testing de MANANTIALES MARKET API", function () {
     const { _body } = response;
     expect(_body.statusCode).to.be.equals(201);
   });
+
   it("Leemos el producto creado", async () => {
     const foundProduct = await productsRepository.readByRepository({
       title: product.title,
@@ -56,6 +74,7 @@ describe("Testing de MANANTIALES MARKET API", function () {
     const { _body } = response;
     expect(_body.statusCode).to.be.equals(200);
   });
+
   it("Actualización de un producto por parte del usuario", async () => {
     const foundProduct = await productsRepository.readByRepository({
       title: product.title,
@@ -74,6 +93,7 @@ describe("Testing de MANANTIALES MARKET API", function () {
     const { _body } = response;
     expect(_body.statusCode).to.be.equals(200);
   });
+
   it("Eliminación de un producto por parte del usuario", async () => {
     const foundProduct = await productsRepository.readByRepository({
       title: product.title,
@@ -84,6 +104,7 @@ describe("Testing de MANANTIALES MARKET API", function () {
     const { _body } = response;
     expect(_body.statusCode).to.be.equals(200);
   });
+
   it("Cerrado de sesión", async () => {
     const response = await requester
       .post("/sessions/logout")
@@ -91,30 +112,65 @@ describe("Testing de MANANTIALES MARKET API", function () {
     const { _body } = response;
     expect(_body.statusCode).to.be.equals(200);
   });
-  it("Eliminación de un producto sin haber inciado sesión", async () => {
+
+  it("Eliminación de un producto sin haber iniciado sesión", async () => {
     const response = await requester.delete(
       "/products/66aa8eeb3077cb2132f4ee75"
     );
     const { _body } = response;
     expect(_body.statusCode).to.be.equals(401);
   });
+
   it("Eliminación de un usuario sin haber iniciado sesión", async () => {
     const foundUser = await usersRepository.readByEmailRepository(user.email);
     const response = await requester.delete("/users/" + foundUser._id);
     const { _body } = response;
     expect(_body.statusCode).to.be.equals(401);
   });
+
   it("Inicio de sesión de un usuario", async () => {
     const response = await requester.post("/sessions/login").send(user);
     const { _body, headers } = response;
     token = headers["set-cookie"][0].split(";")[0];
     expect(_body.statusCode).to.be.equals(200);
   });
+
   it("Eliminación de un usuario", async () => {
     const foundUser = await usersRepository.readByEmailRepository(user.email);
     const response = await requester
       .delete("/users/" + foundUser._id)
       .set("Cookie", token);
+    const { _body } = response;
+    expect(_body.statusCode).to.be.equals(403);
+  });
+
+  it("Registro de un usuario admin", async () => {
+    const response = await requester.post("/sessions/register").send(userAdmin);
+    const { _body } = response;
+    expect(_body.statusCode).to.be.equals(201);
+  });
+
+  it("Inicio de sesión del usuario admin", async () => {
+    const response = await requester.post("/sessions/login").send(userAdmin);
+    const { _body, headers } = response;
+    adminToken = headers["set-cookie"][0].split(";")[0];
+    expect(_body.statusCode).to.be.equals(200);
+  });
+
+  it("Eliminación del usuario no admin", async () => {
+    const foundUser = await usersRepository.readByEmailRepository(user.email);
+    const response = await requester
+      .delete("/users/" + foundUser._id)
+      .set("Cookie", adminToken);
+    const { _body } = response;
+    expect(_body.statusCode).to.be.equals(200);
+  });
+
+  it("Eliminación del usuario admin", async () => {
+    const foundAdminUser = await usersRepository.readByEmailRepository(userAdmin.email);
+    const response = await requester
+      .delete("/users/" + foundAdminUser._id)
+      .set("Cookie", adminToken);
     const { _body } = response;
     expect(_body.statusCode).to.be.equals(200);
   });
